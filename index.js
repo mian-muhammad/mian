4e416d8bbce58882f535101cf222fa1256e58d8e
mian-muhammad <mianmuhammad80@gmail.com>
2020-10-06 17:52:21 +0500

    📦 NEW: First version

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+console.log(`
+Mian Mohammad
+
+Hello I'm Mian Mohammad, a software engineer.
+
+🐦 Twitter: https://twitter.com/mian_se
+📖 GitHub:  https://github.com/mian-muhammad
+🤖 Blog:    http://mianmuhammad.info/
+
+`);
